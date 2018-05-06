d5e154b3605639fdbbd8d62dc9dc9fdec6ace300
hkawabata <13412823+hkawabata@users.noreply.github.com>
2018-05-06 13:38:16 +0900

    update

---
+++ pages/vue/vue_files/MyMessage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>ほげほげ</div>
+</template>
+
+<script>
+    export default {
+        name: 'MyMessage',
+        /*
+        data () {
+            return {
+                msg: 'Hello world!'
+            }
+        }
+        */
+    }
+</script>
+
+<style>
+
+</style>
